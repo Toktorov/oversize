b0f93c3cc5bf6046ead36e8f1b678e1e0f3d3783
Toktorov <ktoktorov2005@gmail.com>
2022-03-22 21:32:54 +0600

    add models

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title 
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
--- apps/settings/models.py
+++ apps/settings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Setting(models.Model):
+    title = models.CharField(max_length=250)
+    decription = models.TextField()
+    logo = models.ImageField(upload_to = 'logo/')
+    tel = models.CharField(max_length=255)
+    email = models.EmailField()
+    facebook = models.CharField(max_length=255)
+    twitter = models.CharField(max_length=255)
+    instagram = models.CharField(max_length=255)
+    linkedin = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title 
+
+    class Meta:
+        verbose_name = "Настройка"
+        verbose_name_plural = "Настройки"
