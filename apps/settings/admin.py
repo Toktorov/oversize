ade980d94aac32636c983cc5f29bd795c307881c
Toktorov <ktoktorov2005@gmail.com>
2022-03-23 02:00:17 +0600

    30% ready

--- apps/settings/admin.py
+++ apps/settings/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from apps.settings.models import Setting
+from apps.settings.models import Setting, Action
 
 # Register your models here.
 admin.site.register(Setting)
+admin.site.register(Action)
--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from apps.settings.models import Setting
+from apps.settings.models import Setting, Action
 from apps.categories.models import Category
 from apps.products.models import Product
 from django.http import HttpResponseNotFound
@@ -14,12 +14,18 @@ def index(request):
     most_popular_products = Product.objects.all().order_by('rating')[:8]
     products = Product.objects.all()[:8]
     random_products = Product.objects.all().order_by('?')[:8]
+    actions = Action.objects.all().order_by('-id')[:4]
+    expensive_products = Product.objects.all().order_by('-price')[:3]
+    one_random_product = Product.objects.all().order_by('?')[:1]
     context = {
         'home' : home,
         'categories' : categories,
         'products' : products,
         'most_popular_products' : most_popular_products,
         'random_products' : random_products,
+        'actions' : actions,
+        'expensive_products' : expensive_products,
+        'one_random_product' : one_random_product,
     }
     return render(request, 'index-2.html', context)
 
