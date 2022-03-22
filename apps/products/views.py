2cff01a9e07b0f16b16958f61f4028bfbb47d97f
Toktorov <ktoktorov2005@gmail.com>
2022-03-22 23:11:19 +0600

    random product detail

--- apps/products/views.py
+++ apps/products/views.py
@@ -6,8 +6,10 @@ from apps.settings.models import Setting
 def product_detail(request, id):
     product = Product.objects.get(id = id)
     home = Setting.objects.latest('id')
+    random_product = Product.objects.all().order_by('?')
     context = {
         'product' : product,
         'home' : home,
+        'random_products' : random_product,
     }
     return render(request, 'product-details.html', context)
